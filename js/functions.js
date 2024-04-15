f26e5b73f6b752a1118ee1f852fff207b1023fe7
Davi Nascimento <davileite2007@gmail.com>
2024-04-15 00:24:57 -0300

    Adicionado um zero extra quando o valor da hora for de 1 dígito

--- js/functions.js
+++ js/functions.js
@@ -45,8 +45,11 @@ $(() => {
         }else{
             var hora = new Date().getHours();
             var minuto = new Date().getMinutes();
+            if(hora < 10){
+                hora = '0' + hora;
+            }
             if(minuto < 10){
-                minuto = '0' + minuto
+                minuto = '0' + minuto;
             }
             scr.append('<div class="entrada-msg"><span>[' + hora + ':' + minuto + '] </span><h4 style="color:' + globalcor + '">' + nick.val().trim() + ':</h4><span> ' + msg.val().trim() + '</span></div>');
             msg.val('');
